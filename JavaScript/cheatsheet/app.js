aeefa4479022fed056cef4dd06cd27d2b2acb2eb
MartinLupa <lupa_martin@hotmail.com>
2021-10-14 13:55:35 +0200

    JS, data types

---
+++ JavaScript/cheatsheet/app.js
@@ -0,0 +1,20 @@
+// Tipos de datos simples / Data types
+
+// Números / Numbers
+number1 = 5;
+number2 = 3.1415921;
+number3 = -10.5;
+number4 = number1 + number3;
+
+//console.log(number1, number2, number3, number4);
+//console.log(typeof number1);
+
+//Strings
+address = "Calle larios Nº 5";
+lastName = "Diaz";
+postTitle = "Teletrabajo, ¿sí o no?";
+console.log(`Vivo en la ${address}, y me llamo Fernando ${lastName}.`);
+
+//Boolean (booleanos o lógicos)
+isAlive = true;
+examPassed = false;
