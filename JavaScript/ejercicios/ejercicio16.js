6bb4a9d20ba54ec2d75bc96b0cc7278e06b3594c
MartinLupa <lupa_martin@hotmail.com>
2021-10-20 08:17:19 +0200

    JS Excercise 16 done, 17 to 23 created

--- JavaScript/ejercicios/ejercicio16.js
+++ JavaScript/ejercicios/ejercicio16.js
@@ -1,1 +1,8 @@
-var prompt = require('prompt-sync')();
+const nums = [2, 6, 8, 13, 1, 7, 4, 8, 10, 25, 3];
+let squareNum = 0;
+
+for (i = 0; i <= 9; i++) {
+  squareNum += nums[i] ** 2;
+}
+
+console.log(squareNum);
