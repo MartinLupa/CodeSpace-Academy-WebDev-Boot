72f05c79c4fee7a2b162693d76142e8609747fe3
MartinLupa <lupa_martin@hotmail.com>
2021-11-03 13:57:46 +0100

    JS Excercises update

--- JavaScript/ejercicios/ejercicio59.js
+++ JavaScript/ejercicios/ejercicio59.js
@@ -1,3 +1,5 @@
 /*
-Se dispone de un array de 5 páginas, 4 filas y 10 columnas, que se refieren al centro, al curso y al número de alumnos de un colegio respectivamente. Imprimir la nota media por curso y la nota media máxima y su centro de pertenencia.
+Se dispone de un array de 5 páginas, 4 filas y 10 columnas, que se refieren al centro, 
+al curso y al número de alumnos de un colegio respectivamente. Imprimir la nota media 
+por curso y la nota media máxima y su centro de pertenencia.
 */
